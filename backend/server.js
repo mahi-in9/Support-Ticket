8a259d22a0b68fb1834fbb9394760c9e6b32b209
Mahendra Kumar <mahithegreat19@gmail.com>
2026-04-01 02:45:26 +0530

    push code

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
   }),
 );
@@ -23,10 +23,8 @@ mongoose
 
 app.use("/api/tickets", ticketRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Api is running on port: ${PORT}`);
 });
-
-// My ticket is not loading in main page. please fix this issue as soon as possible
